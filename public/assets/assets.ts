be5859a392e8cd860732227ea443ef21342ca110
Daniel Wilson <daniel.gregory.wilson@gmail.com>
2022-08-18 13:30:41 -0700

    Move bird to public folder

---
+++ public/assets/assets.ts
@@ -0,0 +1,18 @@
+// Export sheet data to JSON via this method and paste below.
+// https://benborgers.com/posts/google-sheets-json
+// https://opensheet.elk.sh/1KjWh96EXw8-lf8c-MvMb802Wqp8oOlF8ahYRVKxkcWE/Sheet1
+
+export default [
+  {
+    fileName: 'bird',
+    fileNameS: '',
+    fileNameM: '',
+    fileNameLG: '',
+    fileNameXL: '',
+    filetype: '',
+    category: 'fine',
+    tags: 'card|digitalPainting|digital',
+    qualityRating: '6',
+    title: 'Nesting',
+  },
+];
